316ee11f6d936d07379c10a316d94ac0940c6e14
Toni  Diep <adiep123@gmail.com>
2020-05-26 15:14:48 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,20 +6,16 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    # binding.pry
-    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/students/joe-burgess.html"))
-    index_url.each do |student_hash|
-      puts"#{name}, #{location}, #{profile_url}"
+  
+    index_doc = Nokogiri::HTML(open(index_url))
+    student_card = index_doc.css(".student_card")  
+    students = []
+    index_doc.css(".student_card").each do |card|
     end
   end
-
-    #class method. scrapes the student index page ('./fixtures/student-site/index.html')
-    #returns an array in hashes representing one student
-
+   
 
   def self.scrape_profile_page(profile_url)
-    
   end
-
-end
+  end
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,7 @@
 require 'pry'
 class Student
 
-  attr_accessor :name, :location, :profile_url 
-  
-  # :twitter, :linkedin, :github, :blog, :profile_quote, :bio,
+  attr_accessor :name, :location, :profile_url, :twitter, :linkedin, :github, :blog, :profile_quote, :bio
 
   @@all = []
 
@@ -13,13 +11,13 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-      students_array.each do |student_hash| 
-      Student.new(student_hash) 
+      students_array.each do |student_hash| self.new(student_hash) 
     end
   end
 
   def add_student_attributes(attributes_hash)
-
+      attributes_hash.each do |key, value| self.send(("#{key}="), value)
+    end
   end
 
   def self.all
